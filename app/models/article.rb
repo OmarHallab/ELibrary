e0281b75a4945515e31fd8cea18d5475e5a25d0e
Gaetan Osman <cake_geo@hotmail.com>
2015-06-03 15:46:08 -0700

    fixing the views and linking between pages - TODO reading list

--- app/models/article.rb
+++ app/models/article.rb
@@ -16,7 +16,7 @@ class Article < ActiveRecord::Base
   belongs_to :user
 
 #code snippet to upload images
-  has_attached_file :image, styles: { medium: "300x400#"}
+  has_attached_file :image, styles: { medium: "300x400#"} , :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
